81e5c50f8f5ced90ce63359e99bf559dbd7b8ebe
MarchoBoth <anjasariesta01.com>
2024-12-20 01:51:35 +0800

    order

--- src/context/order_context.js
+++ src/context/order_context.js
@@ -52,7 +52,7 @@ export const OrderProvider = ({ children }) => {
   };
 
   const createOrder = async () => {
-    const url = process.env.REACT_APP_API_URL + `/api/orders`;
+    const url = process.env.REACT_APP_API_URL+`/api/order`;
     const config = {
       headers: {
         Authorization: `Bearer ${user?.token || localUser?.token}`,
