df865ea46a9a7d5244e6a65d90a80cdb340b73aa
MarchoBoth <anjasariesta01.com>
2025-01-03 07:17:37 +0800

    fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,6 @@ import React from "react";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import illustration from "images/login-illustration.svg";
 import logo from "images/logo.svg";
-import googleIconImageSrc from "images/google-icon.png";
-import twitterIconImageSrc from "images/twitter-icon.png";
 import { ReactComponent as LoginIcon } from "feather-icons/dist/icons/log-in.svg";
 import { useAuth } from "context/AuthProvider";
 
@@ -13,18 +11,18 @@ const LoginPage = () => {
   const logoLinkUrl = "#";
   const illustrationImageSrc = illustration;
   const headingText = "Sign In To Toko Komputer";
-  const socialButtons = [
-    {
-      iconImageSrc: googleIconImageSrc,
-      text: "Sign In With Google",
-      url: "https://google.com",
-    },
-    {
-      iconImageSrc: twitterIconImageSrc,
-      text: "Sign In With Twitter",
-      url: "https://twitter.com",
-    },
-  ];
+  // const socialButtons = [
+  //   {
+  //     iconImageSrc: googleIconImageSrc,
+  //     text: "Sign In With Google",
+  //     url: "https://google.com",
+  //   },
+  //   {
+  //     iconImageSrc: twitterIconImageSrc,
+  //     text: "Sign In With Twitter",
+  //     url: "https://twitter.com",
+  //   },
+  // ];
   const submitButtonText = "Sign In";
   const forgotPasswordUrl = "#";
   const signupUrl = "#";
